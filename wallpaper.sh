2d8cfe923b9dcfce8ea7c8ba4c50a876a6f5b104
Lautaro Bachmann <laurigbach@gmail.com>
2021-06-21 09:12:11 -0300

    Changed wallpapers location and decreased sleep time between changes

--- wallpaper.sh
+++ wallpaper.sh
@@ -2,7 +2,7 @@
 echo "This works"
 while true
 do
-	feh --bg-fill --randomize ~/Pictures/cristibook-wallpapers
-	sleep 15m
+	feh --bg-fill --randomize /usr/share/backgrounds/arcolinux
+	sleep 5m
 
 done
